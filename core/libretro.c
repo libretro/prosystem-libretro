1cf54722a37e30a82a16db2a4fbb5edcbe005c70
twinaphex <libretro@gmail.com>
2017-01-24 15:39:57 +0100

    Better error handling

--- core/libretro.c
+++ core/libretro.c
@@ -247,12 +247,6 @@ void retro_cheat_set(unsigned index, bool enabled, const char *code)
 bool retro_load_game(const struct retro_game_info *info)
 {
    enum retro_pixel_format fmt = RETRO_PIXEL_FORMAT_XRGB8888;
-   if (!environ_cb(RETRO_ENVIRONMENT_SET_PIXEL_FORMAT, &fmt))
-   {
-      if (log_cb)
-         log_cb(RETRO_LOG_INFO, "[ProSystem]: XRGB8888 is not supported.\n");
-      return false;
-   }
 
    struct retro_input_descriptor desc[] = {
       { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_LEFT,  "Left" },
@@ -277,7 +271,18 @@ bool retro_load_game(const struct retro_game_info *info)
       { 0 },
    };
 
+   if (!info)
+      return false;
+
    environ_cb(RETRO_ENVIRONMENT_SET_INPUT_DESCRIPTORS, desc);
+
+   if (!environ_cb(RETRO_ENVIRONMENT_SET_PIXEL_FORMAT, &fmt))
+   {
+      if (log_cb)
+         log_cb(RETRO_LOG_INFO, "[ProSystem]: XRGB8888 is not supported.\n");
+      return false;
+   }
+
 
    memset(keyboard_data, 0, sizeof(keyboard_data));
 
